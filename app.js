0b2ab9d4c9c9abf6c5d49b4af72ebef6f2f2c09e
Suryansh <suryanshgupta6502@gmail.com>
2023-08-23 11:45:42 +0530

    .

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -17,12 +16,10 @@ var posts=[]
 
 
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "views"));
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname+"public"));
+app.use(express.static("public"));
+
 
 
 app.get("/", function (req, res) {
